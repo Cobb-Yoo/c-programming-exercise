bb64f167b37cc9b00b57f0ade113b75e36748ed4
YooCobb <yhj010597@naver.com>
2019-12-02 13:28:54 +0900

    깃에 잘못올려서 깨진 부분 수정함 한글깨진거 복구해야함

--- chapter 9/9-10.cpp
+++ chapter 9/9-10.cpp
@@ -21,73 +21,37 @@ class Shape{
 	public:
 		Shape(){
 			this->link = NULL;
-<<<<<<< HEAD
 		}
 		virtual void show()=0;
 		void setLink(Shape *n){
 			this->link = n;
 		}
-=======
-			name = "";
-		}
-		virtual void show(int n)=0;
-		void setName(int n);
->>>>>>> 1508d44f6396d5f041bcab91a323ffb31e1797e2
 		Shape *getLink(){
 			return this->link;
 		}
 };
 
-void Shape::setName(int n){
-	switch (n){
-		case 1:
-			this->name = "Line";
-			break;
-		case 2:
-			this->name = "Circle";
-			break;
-		case 3:
-			this->name = "Rect";
-			break;
-	}
-}
-
 class Line:public Shape{
 	public:
-<<<<<<< HEAD
 		Line():Shape(){}
 		void show(){
 			cout << ": Line\n";
-=======
-		Line():Shape(){
-			setName(1);
->>>>>>> 1508d44f6396d5f041bcab91a323ffb31e1797e2
 		}
 };
 
 class Circle:public Shape{
 	public:
-<<<<<<< HEAD
 		Circle():Shape(){}
 		void show(){
 			cout << ": Circle\n";
-=======
-		Circle():Shape(){
-			setName(2);
->>>>>>> 1508d44f6396d5f041bcab91a323ffb31e1797e2
 		}
 };
 
 class Rect:public Shape{
 	public:
-<<<<<<< HEAD
 		Rect():Shape(){}
 		void show(){
 			cout << ": Rect\n";
-=======
-		Rect():Shape(){
-			setName(3);
->>>>>>> 1508d44f6396d5f041bcab91a323ffb31e1797e2
 		}
 };
 
@@ -102,17 +66,15 @@ class GraphicEditor{
 		}
 		void ins(int n);
 		void del(int n);
-<<<<<<< HEAD
 		void view();
 };
 
 void GraphicEditor::view(){
-	Shape *head = this->pStart;
 	for(;;){
-		if(head == this->pLase) break;
+		if(this->pStart == this->pLase) break;
 		
-		head->show();
-		head = head->getLink();
+		pStart->show();
+		pStart = pStart->getLink();
 	}
 }
 
@@ -130,49 +92,23 @@ void GraphicEditor::ins(int n){
 			break;
 	}
 	
-	
 	if(this->pStart == NULL && this->pLase == NULL){ // ¸®½ºÆ®¿¡ Ã³À½ »ðÀÔÇÏ´Â °æ¿ì 
-		this->pStart->setLink(tmp);
-		this->pLase->setLink(tmp)z;
-	}
-	else{
-		for(;;){
-			if(this->pStart->getLink() == NULL){
-				this->pStart->setLink(tmp);
-				this->pLase->setLink(tmp);
-			}
-			else {
-				this->pStart = this->pStart->getLink();
-			}
-=======
-};
-
-void GraphicEditor::ins(int n){
-	switch (n){
-		case 1:
-			Line tmp;
-			break;
-		case 2:
-			Circle tmp;
-			break;
-		case 3:
-			Rect tmp;
-			break;
-	}
-	
-	Shape *head = this->pStart;
-	
-	if(this->pStart == NULL && this->pLase == NULL){ // ë¦¬ìŠ¤íŠ¸ì— ì²˜ìŒ ì‚½ìž…í•˜ëŠ” ê²½ìš° 
 		this->pStart = tmp;
 		this->pLase = tmp;
 	}
 	else{
+		Shape *head = this->pStart;
 		for(;;){
-			
->>>>>>> 1508d44f6396d5f041bcab91a323ffb31e1797e2
+			if(head->getLink() == this->pLase){
+				head->setLink(tmp);
+				this->pLase->setLink(tmp);
+			}
+			else {
+				head = head->getLink();
+			}
 		}
-	} 
-}
+	}
+};
 
 int main(){
 	GraphicEditor *graphic = new GraphicEditor();
@@ -180,7 +116,6 @@ int main(){
 	int n;
 	while(UI::menu(n) && n!=4){
 		switch (n){
-<<<<<<< HEAD
 			case 1://»ðÀÔ 
 				UI::shapeMenu(n);
 				graphic->ins(n);
@@ -190,14 +125,6 @@ int main(){
 			case 3: //¸ðµÎº¸±â
 				graphic->view();
 				break;
-=======
-			case 1:
-				UI::shapeMenu(n);
-				switch (n){
-					case 1:
-						break;
-				}
->>>>>>> 1508d44f6396d5f041bcab91a323ffb31e1797e2
 		}
 	}
 }
